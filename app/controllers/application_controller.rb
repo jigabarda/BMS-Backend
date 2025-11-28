7fbdf11c7b94ba0384f4a1984238c2e7c95a5022
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-11-28 14:07:59 +0800

    enhance authentication handling and include JWT current user logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  include Devise::Controllers::Helpers   # allows current_user, authenticate_user!, etc.
+  respond_to :json
+
+  # If you want to protect globally:
+  # before_action :authenticate_user!
 end
