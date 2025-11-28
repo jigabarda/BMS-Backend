7e9b82359fcdd68409d70706d06c0e2c6cf6ac3e
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-11-28 14:10:29 +0800

    extend User model with JWT authentication and device association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 # app/models/user.rb
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist # we'll create revoke model
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :validatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
 
-  has_many :broadcasts, dependent: :nullify
   has_many :devices, dependent: :destroy
+  has_many :broadcasts, dependent: :destroy
 end
